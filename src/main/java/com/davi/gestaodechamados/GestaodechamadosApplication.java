c0eff25381c525ea8015dfc3fc8fe7f9450568cb
davi7709 <davialealves@gmail.com>
2026-08-25 22:51:54 -0300

    Initial commit

---
+++ src/main/java/com/davi/gestaodechamados/GestaodechamadosApplication.java
@@ -0,0 +1,13 @@
+package com.davi.gestaodechamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaodechamadosApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GestaodechamadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davi/gestaodechamados/GestaodechamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.gestaodechamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaodechamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
