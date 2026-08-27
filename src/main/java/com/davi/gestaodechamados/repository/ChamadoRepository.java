8c88b2b54f134302cbf4a6109bdda10ac63c572e
davi7709 <davialealves@gmail.com>
2026-08-27 19:30:55 -0300

    Camada de repositorio

---
+++ src/main/java/com/davi/gestaodechamados/repository/ChamadoRepository.java
@@ -0,0 +1,11 @@
+package com.davi.gestaodechamados.repository;
+
+import com.davi.gestaodechamados.enums.Status;
+import com.davi.gestaodechamados.model.Chamado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ChamadoRepository extends JpaRepository<Chamado, Long> {
+    List<Chamado> chamadoPorStatus(Status status);
+}
